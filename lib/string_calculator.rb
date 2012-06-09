90ef0997492a95e45869aa6c93fc53b5d45b4606
Miguel San Román <endymiion@gmail.com>
2012-06-09 22:12:20 +0200

    Now it handles also custom delimiters

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,8 +1,17 @@
 class StringCalculator
 
 		def add(numbers)
-			return numbers.split(/[,\\n]/).inject(0) {|sum, number|
+			custom_delimiter = get_custom_delimiter(numbers)
+			regexp = "[,\\\n#{custom_delimiter}]"
+			result = numbers.split(/#{regexp}/).inject(0) {|sum, number|
 				sum + number.to_i
 			}
+			return result
+		end
+
+		def get_custom_delimiter(numbers)
+			if numbers.start_with? "//"
+				return numbers[2...numbers.index("\n")]
+			end
 		end
 end
--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -4,18 +4,22 @@ require 'string_calculator'
 describe 'String calculator' do
 	
 	it 'Returns 0 for no input' do
-		StringCalculator.new.add('').should == 0
+		StringCalculator.new.add("").should == 0
 	end
 
 	it 'Returns the number passed as input for one number' do
-		StringCalculator.new.add('1').should == 1
+		StringCalculator.new.add("1").should == 1
 	end
 
 	it 'For any amount of numbers separated by comma will return its sum' do
-		StringCalculator.new.add('1,2,3,4').should == 10
+		StringCalculator.new.add("1,2,3,4").should == 10
 	end
 
-	it 'Can handle \n instead of commas' do
-		StringCalculator.new.add('1\n2,3').should == 6
+	it 'Handles \n as delimiters too' do
+		StringCalculator.new.add("1\n2,3").should == 6
+	end
+
+	it 'Handles custom delimiters' do
+		StringCalculator.new.add("//;\n1;2").should == 3
 	end
 end
