c59504ee3a5976c240a32b64ae5b7a5ce133780b
Miguel San Román <endymiion@gmail.com>
2012-06-09 21:34:18 +0200

    Passing scenario for one number

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,6 +1,7 @@
 class StringCalculator
 
 		def add(numbers)
+			return numbers.to_i unless numbers.empty?
 			0
 		end
 end
--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -6,4 +6,8 @@ describe 'String calculator' do
 	it 'Returns 0 for no input' do
 		StringCalculator.new.add('').should == 0
 	end
+
+	it 'Returns the number passed as input for one number' do
+		StringCalculator.new.add('1').should == 1
+	end
 end
