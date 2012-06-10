e702d51d9b6c8ae2daab7ac6ee7a1e89a78bf3b8
Miguel San Román <endymiion@gmail.com>
2012-06-10 02:13:47 +0200

    Handles custom delimiters with format [] and did a huge refactor

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,30 +1,65 @@
+class StringSplitter
+
+	def split_to_i(string)
+		custom_delimiter = get_custom_delimiter(string)
+		regexp = "[,\\\n#{custom_delimiter}]"
+		string = trim_custom_delimiter(string, custom_delimiter)
+		result = string.split(/#{regexp}/).select{|element| not element.empty?}
+	end
+
+	def trim_custom_delimiter(string, custom_delimiter)
+		unless custom_delimiter.nil?
+			string.slice!(0..2+custom_delimiter.length)
+		end
+		string
+	end
+
+	def get_custom_delimiter(string)
+		if string.start_with? "//"
+			return string[2...string.index("\n")]
+		end
+	end
+end
+
+class String
+	def is_number?
+  	match(/\A[+-]?\d+?(_?\d+)*(\.\d+e?\d*)?\Z/) == nil ? false : true
+  end
+end
+
+class NumberCollection < Array
+
+	def initialize(collection)
+		collection.each{|element|
+			self << element.to_i if element.is_number?
+		}
+	end
+
+	def negatives
+		self.select{|number| number < 0}
+	end
+end
+
 class StringCalculator
 
-		def add(numbers)
-			custom_delimiter = get_custom_delimiter(numbers)
-			regexp = "[,\\\n#{custom_delimiter}]"
-			numbers = numbers.split(/#{regexp}/)
-			negatives = negatives(numbers)
-			raise Exception.new("Negatives not allowed: #{negatives.inspect}") if negatives.any?
-			numbers.inject(0) {|sum, number|
-				if is_too_big? number.to_i
-					sum + 0
-				else sum + number.to_i
-				end
-			}
-		end
+	MAXIMUM_NUMBER = 1000
 
-		def is_too_big? (number)
-			number > 1000
-		end
+	def initialize(splitter)
+		@splitter = splitter
+	end
 
-		def negatives(numbers)
-			numbers.select{|number| number.to_i < 0}
-		end
+	def add(numbers)
+		numbers = NumberCollection.new(@splitter.split_to_i(numbers))
+		negatives = numbers.negatives
+		raise Exception.new("Negatives not allowed: #{negatives.inspect}") if negatives.any?
+		small_enough(numbers).inject(0){|sum, number| sum + number}
+	end
 
-		def get_custom_delimiter(numbers)
-			if numbers.start_with? "//"
-				return numbers[2...numbers.index("\n")]
-			end
-		end
+	def small_enough(numbers)
+		numbers.select{|number| not is_too_big? number}
+	end
+
+	def is_too_big? (number)
+		number > MAXIMUM_NUMBER
+	end
 end
--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -3,33 +3,77 @@ require 'string_calculator'
 
 describe 'String calculator' do
 	
+	before(:each) do
+		@splitter = double("StringSplitter")
+		@calculator = StringCalculator.new(@splitter)
+	end
+	
 	it 'returns 0 for no input' do
-		StringCalculator.new.add("").should == 0
+		@splitter.stub(:split_to_i).and_return(([]))
+		@calculator.add("").should == 0
 	end
 
 	it 'returns the number passed as input for one number' do
-		StringCalculator.new.add("1").should == 1
+		@splitter.stub(:split_to_i).and_return((["1"]))
+		@calculator.add("1").should == 1
 	end
 
-	it 'returns the sum of all separated by comma numbers provided' do
-		StringCalculator.new.add("1,2,3,4").should == 10
+	it 'gets the splitted numbers from the StringSplitter class' do
+		@splitter.should_receive(:split_to_i).with("1,2,3,4").and_return((["1","2","3","4"]))
+		@calculator.add("1,2,3,4")
 	end
 
-	it 'handles \n as delimiters too' do
-		StringCalculator.new.add("1\n2,3").should == 6
+	it 'returns the sum of all numbers provided' do
+		@splitter.stub(:split_to_i).and_return((["1","2","3","4"]))
+		@calculator.add("1,2,3,4").should == 10
 	end
 
-	it 'handles custom delimiters' do
-		StringCalculator.new.add("//;\n1;2").should == 3
-	end
 
 	it 'raises an exception when passing negative numbers' do
+		@splitter.stub(:split_to_i).and_return((["-1","2"]))
 		expect {
-			StringCalculator.new.add("-1,2")
-			}.to raise_error ('Negatives not allowed: ["-1"]')
+			@calculator.add("-1,2")
+			}.to raise_error ('Negatives not allowed: [-1]')
 	end
 
 	it 'ignores numbers greater than 1000' do
-		StringCalculator.new.add("2,1001").should == 2
+		@splitter.stub(:split_to_i).and_return((["2","1001"]))
+		@calculator.add("2,1001").should == 2
+	end
+end
+
+describe "StringSplitter" do
+
+	before(:each) do
+		@splitter = StringSplitter.new
+	end
+
+	it 'handles commas as delimiters' do
+		@splitter.split_to_i("1,2,3,4").should == ["1","2","3","4"]
+	end
+
+	it 'handles \n as delimiters too' do
+		@splitter.split_to_i("1\n2,3").should == ["1","2","3"]
+	end
+
+	it 'handles custom delimiters' do
+		@splitter.split_to_i("//;\n1;2").should == ["1","2"]
+	end
+
+	it 'handles delimiters of any size' do
+		@splitter.split_to_i("//[***]\n1***2***3").should == ["1","2","3"]
+	end
+end
+
+describe "NumberCollection" do
+
+	it 'gets an string collection and stores as numbers' do
+		collection = NumberCollection.new(["1","2","3","4"])
+		collection.should == [1,2,3,4]
+	end
+
+	it 'returns the subset of negative numbers on negatives method' do
+		collection = NumberCollection.new(["-1","2"])
+		collection.negatives.should == [-1]
 	end
 end
